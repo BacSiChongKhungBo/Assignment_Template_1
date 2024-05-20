6b7a120b44b784e804ebddd191a37348228dbef6
BacSiChongKhungBo <huywewe123@gmail.com>
2024-05-20 13:06:44 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using Assignment_Template_1.View;
+
+namespace Assignment_Template_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Present p = new Present();
+            p.Menu();
+        }
+    }
+}
---
+++ View/Present.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_Template_1.View
+{
+    internal class Present
+    {
+        public Present()
+        {
+            
+        }
+
+        public void Menu()
+        {
+            Console.WriteLine("Menu code vào đây");
+        }
+    }
+}
